7981733abcf137776e0da84c542375ad9ef9d63f
Alex Sinagin <a.sinyagin996@gmail.com>
2024-04-13 21:24:16 +0300

    add code in program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Ввод массива строк с клавиатуры
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string input = Console.ReadLine()!;
+        string[] inputArray = input.Split(',')!;
+
+        // Формирование нового массива из строк длиной <= 3 символов
+        string[] newArray = new string[inputArray.Length];
+        int count = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                newArray[count] = str;
+                count++;
+            }
+        }
+
+        // Вывод нового массива на экран
+        Console.WriteLine("Новый массив из строк длиной <= 3 символов:");
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(newArray[i] + " ");
+        }
